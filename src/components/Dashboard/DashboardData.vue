2b5dc4cf00708727d5b1e5930e7e1d30667bf879
Stanislaw Daniluk <stanislaw.d@icloud.com>
2022-07-12 10:40:48 +0200

    Fixing DataTable values validatio

--- src/components/Dashboard/DashboardData.vue
+++ src/components/Dashboard/DashboardData.vue
@@ -147,7 +147,7 @@ export default {
       protein: 0,
     },
     valueRules: [
-      (v) => /^[0-9,]*$/.test(v) || "Should contain only numbers",
+      (v) => /^[0-9,.]*$/.test(v) || "Should contain only numbers",
       (v) => !!v || "Value is required",
     ],
     nameRules: [(v) => !!v || "Product name is required"],
